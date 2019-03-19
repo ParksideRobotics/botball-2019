65886374aa42b4a572c85515bc2c37bb02e3a487
schineaj23 <schineaj23@pwcs-edu.org>
2019-03-19 16:14:09 -0400

    updated get-sh

--- .build/get-deps.sh
+++ .build/get-deps.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+sudo apt-get update # update just in case
+sudo apt-get install libzbar-dev libopencv-dev libjpeg-dev python-dev doxygen swig # get dependencies for libwallaby
 git clone https://github.com/kipr/libwallaby.git
 cd libwallaby
 mkdir build
